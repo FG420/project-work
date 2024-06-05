f65e3d7b82d5473b918a3894e6ac5de49f81180c
Godwin <agblezeg@gmail.com>
2024-06-05 12:27:39 +0200

    Add Home Page and Dark Mode

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,37 @@
+import { Button } from "./ui/button";
+import "./sidebar.css";
+import { ModeToggle } from "./ui/theme-toggle";
+
+export default function Sidebar() {
+  return (
+    <>
+      <div className="sidebar">
+        <Button className="sidebar-btn" variant={"outline"}>
+          Items
+        </Button>
+        <Button className="sidebar-btn" variant={"outline"}>
+          Suppliers
+        </Button>
+        <Button className="sidebar-btn" variant={"outline"}>
+          Purchases
+        </Button>
+        <Button className="sidebar-btn" variant={"outline"}>
+          Orders
+        </Button>
+        <Button className="sidebar-btn" variant={"outline"}>
+          Sales Analysis
+        </Button>
+
+        <div className="lower-sidebar">
+          <div className="toggle">
+            <ModeToggle />
+          </div>
+
+          <Button className="sign-out-btn sidebar-btn" variant={"outline"}>
+            Sign Out
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
