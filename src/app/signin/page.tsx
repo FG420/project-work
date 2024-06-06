f00fef476eb9124b184bbabbc266fa2f8f95f4ab
Godwin <agblezeg@gmail.com>
2024-06-06 10:01:16 +0200

    Rename interfaces and add google captcha

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,50 +1,55 @@
 'use client'
 
 import LoginPage from "@/components/login";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import SignUpPage from "@/components/signup";
+import { Card } from "@/components/ui/card";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
 
 
-export default function SignInPage () {
+export default function SignInPage() {
+  const router = useRouter();
 
-    // Working!
-    // const timer = setTimeout( () => {
-    //     alert( "30 seconds passed!\nForm Resetting!" )
-    //     window.location.reload()
-    // }, 30000 )
+  // const [showMessage, setShowMessage] = useState(false);
 
+  const timer = setTimeout(() => {
+    setShowMessage(true);
+  }, 5000);
 
-    return (
-        <main className="flex justify-center items-center h-screen">
-            <div >
-                <div >
-                    <Card >
-                        <CardHeader className="text-center font-semibold text-xl pt-10">
-                            Welcome Back
-                        </CardHeader>
-                        <CardContent>
-                            <LoginPage />
-                        </CardContent>
-                        <p className="flex items-center justify-center pt-0 pb-10">
-                            <Link
-                                className=" transition-all text-red-500 hover:text-red-300
-                                    hover:transition-all"
-                                href={ "/forgotpass" }
-                            >
-                                Forgot Password
-                            </Link>
-                        </p>
-                    </Card>
-                </div>
-                <p className="text-center p-6">Not Register Yet? <br />
-                    <Link
-                        className="p-2 flex items-center justify-center transition-all text-red-500 hover:text-red-300
-                            hover:transition-all"
-                        href={ "/signup" }
-                    >
-                        Sign Up
-                    </Link>
-                </p>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Sign up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card className="">
+            <LoginPage></LoginPage>
+          </Card>
+          <div className="flex justify-center">
+            <Link
+              href={"/forgotpass"}
+              className=" transition-all font-semibold hover:text-red-500 hover:transition-all p-4"
+            >
+              Forgot Password
+            </Link>
+          </div>
+
+          {/* {showMessage === true ? (
+            <div className="">
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button>Ciao</Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogContent>
+              </Dialog>
             </div>
         </main>
     )
