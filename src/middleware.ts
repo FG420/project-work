64216e851d4986f45be3446140216e075583b53e
FG420 <facchinatog97@gmail.com>
2024-06-07 14:13:22 +0200

    shoutdown middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
+export function middleware ( req: NextRequest ) {
     const path = req.nextUrl.pathname
-    const isPublicPath = path === '/' || path === '/:path*'
-    const token = req.cookies.get('token')?.value || ''
+    const isPublicPath = path === '/ok' || path === '/hi'
+    const token = req.cookies.get( 'token' )?.value || ''
 
-    if (isPublicPath) {
-        return NextResponse.redirect(new URL('/signin', req.nextUrl));
+    if ( isPublicPath ) {
+        return NextResponse.redirect( new URL( '/signin', req.nextUrl ) );
     }
 
     // if (!isPublicPath) {
@@ -18,5 +18,5 @@ export function middleware(req: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/', '/signin', '/signup', '/dashboard/:path*' ]
+    matcher: [ '/', '/signin', '/signup', '/dashboard/:path*' ]
 }
