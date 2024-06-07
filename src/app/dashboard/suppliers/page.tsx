07d97dd1aaa2462eb2d18a766f2d862d0e36c70a
FG420 <facchinatog97@gmail.com>
2024-06-07 16:10:42 +0200

    suppliers ok

--- src/app/dashboard/suppliers/page.tsx
+++ src/app/dashboard/suppliers/page.tsx
@@ -1,3 +1,21 @@
-export default function Suppliers() {
-  return <h1>Suppliers Page</h1>;
+
+import { SupplierT, suppliers } from "@/lib/suppliers";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+
+async function getData (): Promise<SupplierT[]> {
+    // Fetch data from your API here.
+    return suppliers
 }
+
+export default async function Suppliers () {
+    const data = await getData()
+    return (
+        <main>
+            <DataTable columns={ columns } data={ data } />
+
+        </main>
+    )
+}
+
+
