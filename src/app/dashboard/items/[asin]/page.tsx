ab619117adbfdced51c7fbe70ea599cd75c2b41d
Godwin <agblezeg@gmail.com>
2024-06-11 15:20:50 +0200

    Add api call for items

--- src/app/dashboard/items/[asin]/page.tsx
+++ src/app/dashboard/items/[asin]/page.tsx
@@ -3,62 +3,48 @@ import fs from "fs";
 import Image from "next/image";
 import path from "path";
 
-const item = {
-    ASIN: "B07D9SB7XW",
-    Title: "Minecraft - Videogioco Nintendo - Ed. Italiana - Versione su scheda",
-    Giacenza: 100,
-    CategoriaID: 1
-};
-
 const ItemPage = ({ params }) => {
-    
-    const { asin } = params;
+  console.log(params);
+  const { asin } = params;
 
-    const imageDir = path.join(process.cwd(), 'public', 'images', asin)
+  const imageDir = path.join(process.cwd(), "public", "images", asin);
 
-    let images = [];
+  let images: string[] = [];
 
-    try {
-        images = fs.readdirSync(imageDir).filter(file => /^Image\d+\.jpg$/.test(file));
-    } catch (error) {
-        console.error('Failed to load images:', error);
-    }
+  try {
+    images = fs.readdirSync(imageDir).filter((file) => /^Image\d+\.jpg$/.test(file));
+  } catch (error) {
+    console.error("Failed to load images:", error);
+  }
 
-    return (
-        <div className="flex items-center" style={{ border: '3px solid black'}}>
-            <h1>Item Page</h1>
-            <p>ASIN: {asin}</p>
+  return (
+    <div className="flex items-center" style={{ border: "3px solid black" }}>
+      <h1>Item Page</h1>
+      <p>ASIN: {asin}</p>
 
-            {images.length > 0 ? (
-                <Carousel style={{ border: '1px solid red', width: '500px', height: '500px' }}>
-                    <CarouselContent>
-
-                    {images.map((filename, index) => (
-                        <CarouselItem key={index}>
-                            <Image src={`/images/${asin}/${filename}`} style={{ height: '500px !important'}} width={500} height={200} alt={`Item ${index + 1}`} />
-                        </CarouselItem>
-                    ))}
-                    </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                </Carousel>
-            ) : (
-                <p>No images found.</p>
-            )}
-        </div>
-    );
+      {images.length > 0 ? (
+        <Carousel style={{ border: "1px solid red", width: "500px", height: "500px" }}>
+          <CarouselContent>
+            {images.map((filename, index) => (
+              <CarouselItem key={index}>
+                <Image
+                  src={`/images/${asin}/${filename}`}
+                  style={{ height: "500px !important" }}
+                  width={500}
+                  height={200}
+                  alt={`Item ${index + 1}`}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      ) : (
+        <p>No images found.</p>
+      )}
+    </div>
+  );
 };
 
 export default ItemPage;
-
-/*
-
-            {images.length > 0 ? (
-                images.map((filename, index) => (
-                    <img key={index} src={`/images/${asin}/${filename}`} alt={`Item ${index + 1}`} />
-                ))
-            ) : (
-                <p>No images found.</p>
-            )}
-
-            */
--- src/app/dashboard/items/page.tsx
+++ src/app/dashboard/items/page.tsx
@@ -1,45 +1,67 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
-import { Item } from "@/lib/interfaces";
-import { items } from "@/lib/items";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Item } from "@/lib/types";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Items() {
+  const [items, setItems] = useState<Item[]>([]);
   const router = useRouter();
 
-  function trimTitle(title: string, maxLength: number = 30) {
+  function trimTitle(title: string, maxLength: number = 40) {
     if (title.length > maxLength) {
       return title.substring(0, maxLength) + "...";
     }
     return title;
   }
 
-  function clickItem(item: any) {
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const result = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL!}/Item`);
+        setItems(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  function clickItem(item: Item) {
     console.log(item);
 
-    // Navvigate to item page with the ASIN
-    router.push(`/dashboard/items/${item.ASIN}`);
+    router.push(`/dashboard/items/${item.asin}`);
   }
 
   return (
     <>
-      <h2 className="text-2xl">Items Page</h2>
+      <h2 className="text-2xl">Items Inventory</h2>
 
-      <div className="items-container" style={{ display: "flex", gap: "2rem", flexWrap: "wrap" }}>
+      <div
+        className="items-container"
+        style={{
+          display: "flex",
+          gap: "2rem",
+          flexWrap: "wrap",
+          justifyContent: "space-between"
+        }}
+      >
         {items.map((item) => (
           <Card
-            style={{ padding: "1rem", maxWidth: "320px", cursor: "pointer", display: "flex", gap: "10px" }}
-            key={item.ASIN}
+            style={{ padding: "1rem", width: "340px", cursor: "pointer", display: "flex", gap: "10px" }}
+            key={item.asin}
             onClick={() => clickItem(item)}
           >
-            <Image src={"/images/B07D9SB7XW/Image1.jpg"} width={64} height={64} alt="test" />
+            <Image src={`/images/${item.asin}/Image1.jpg`} width={64} height={64} alt="Item Image" />
             <span>
-              <h1>{trimTitle(item.Title)}</h1>
-              <p>ASIN: {item.ASIN}</p>
-              <p>Giacenza: {item.Giacenza}</p>
-              <p>Categoria: {item.CategoriaID}</p>
+              <p className="font-semibold">{trimTitle(item.title)}</p>
+              <p>ASIN: {item.asin}</p>
+              <p>Giacenza: {item.stock}</p>
+              <p>Categoria: {item.categoryName}</p>
             </span>
           </Card>
         ))}
