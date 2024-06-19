c2ab9ddc70d2aafe548ac8426f98c20c8c81e906
MauroMacu <maculanmauro652@gmail.com>
2024-06-19 12:14:04 +0200

    add sum€ of items on the dawer

--- src/components/table/order-items.tsx
+++ src/components/table/order-items.tsx
@@ -6,6 +6,14 @@ import { Button } from "@/components/ui/button"
 import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "../ui/drawer";
 
 export default function OrderItems({ items }: {items: OrderItem[]}) {
+
+  const calculateTotalPrice = () => {
+    return items.reduce((total, item) => {
+      return total + (item.itemPrice * item.quantityOrdered);
+    }, 0)
+  }
+  const totalPrice = calculateTotalPrice()
+
   return (
     <Drawer>
       <DrawerTrigger asChild>
@@ -16,17 +24,21 @@ export default function OrderItems({ items }: {items: OrderItem[]}) {
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm">
           <DrawerHeader>
-            <DrawerTitle>Order Items</DrawerTitle>
-            <DrawerDescription>List of items in the order</DrawerDescription>
+            <DrawerTitle>Order Items
+              <span className="ml-5 text-sm font-normal justify-between">
+                 Totale {new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(totalPrice)}
+              </span>
+            </DrawerTitle>
+            <DrawerDescription>Prodotti Ordinati</DrawerDescription>
           </DrawerHeader>
-          <div className="p-4 h-80 overflow-y-auto" >
+          <div className="p-4 h-80 overflow-y-auto">
             {items && items.length > 0 ? (
               items.map((item) => (
                 <div key={item.orderItemID} className="border-b pb-2 mb-2">
                   <div><strong>ASIN:</strong> {item.asin}</div>
                   <div><strong>Title:</strong> {item.title}</div>
                   <div><strong>Quantity Ordered:</strong> {item.quantityOrdered}</div>
-                  <div><strong>Item Price:</strong> {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(item.itemPrice)}</div>
+                  <div><strong>Item Price:</strong> {new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(item.itemPrice)}</div>
                 </div>
               ))
             ) : (
