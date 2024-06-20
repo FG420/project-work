a594e1c4e877c2d3515ea3e28ece296914069e57
Godwin <agblezeg@gmail.com>
2024-06-20 12:36:31 +0200

    Implement initial graph

--- src/app/dashboard/sales-analysis/page.tsx
+++ src/app/dashboard/sales-analysis/page.tsx
@@ -1,3 +1,10 @@
+import BarChart from '@/components/charts/bar-chart';
+
 export default function SaleAnalysis() {
-  return <h1>Sale Analysis Page</h1>;
+  return (
+    <>
+      <h1>Sale Analysis Page</h1>
+      <BarChart />
+    </>
+  );
 }
