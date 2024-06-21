b4f98afb1e24c4f92e3932277dea5f73f4016a3e
FG420 <facchinatog97@gmail.com>
2024-06-21 10:10:55 +0200

    component filtered-items created for homepage

---
+++ src/components/filtered-items/filtered-component.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import axiosInstanceClient from '@/lib/axios-client';
+import { Item } from '@/lib/types';
+import { useEffect, useState } from 'react';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+import { ScrollArea } from '../ui/scroll-area';
+import { Label } from '../ui/label';
+import { FilteredDataTable } from './filtered-data-table';
+import { columns } from './filtered-columns';
+
+export default function TopItemsComponent() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const fetchItems = async () => {
+    try {
+      const getItems = await axiosInstanceClient.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/Item`,
+      );
+
+      const itemsFilterByStock = getItems.data.filter((item: any) => item.stock < 150).sort((a: any, b: any) => b.stock - a.stock);
+
+      setItems(itemsFilterByStock);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <main>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Items with Stock Below 150</CardTitle>
+          <CardDescription>All the items with stock lower than 150</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <FilteredDataTable data={items} columns={columns}/>
+        </CardContent>
+        <CardFooter className="flex justify-between"></CardFooter>
+      </Card>
+    </main>
+  );
+}
---
+++ src/components/top-suppliers.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import axiosInstanceClient from '@/lib/axios-client';
+import { Supplier } from '@/lib/types';
+import { useState, useEffect } from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { Label } from './ui/label';
+
+export default function HomeComeponent() {
+  const [suppliers, setSuppliers] = useState<Supplier[]>([]);
+
+  const getSuppliers = async () => {
+    const getPurchaseSuppliers = await axiosInstanceClient.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/Purchase`,
+    );
+
+    const getSupps = await axiosInstanceClient.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/Supplier`,
+    );
+
+    console.log(getSupps.data)
+
+    // console.log(
+    //   getPurchaseSuppliers.data.map((purchase) =>
+    //     purchase.purchasedItems
+    //       .map((item) => item.price * item.quantity)
+    //       .reduce((partial: any, a: any) => partial + a, 0),
+    //   )
+    // );
+
+    // console.log(
+    //   getPurchaseSuppliers.data
+    //   .filter((purchase) => purchase.purchasedItems.length >= 3)
+    //   .map((supplier) => supplier.supplier)
+    // );
+
+    setSuppliers(
+      getPurchaseSuppliers.data.map((supplier: { supplier: any }) => supplier.supplier),
+    );
+  };
+
+  useEffect(() => {
+    getSuppliers();
+  }, []);
+
+  return (
+    <main>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Top 3 Suppliers</CardTitle>
+          <CardDescription>The top 3 suppliers from the purchases</CardDescription>
+        </CardHeader>
+        {/* <CardContent>
+      {suppliers.map((supp) => (
+        <div key={supp.supplierID}>
+          <Label>{supp.description}</Label>
+        </div>
+      ))}
+      
+      </CardContent> */}
+        <CardFooter className="flex justify-between"></CardFooter>
+      </Card>
+    </main>
+  );
+}
