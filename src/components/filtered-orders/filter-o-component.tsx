4bd15e6837673a194249d589eef2353b749ab323
FG420 <facchinatog97@gmail.com>
2024-06-22 11:16:51 +0200

    merge main into Gianluca

--- src/components/filtered-orders/filter-o-component.tsx
+++ src/components/filtered-orders/filter-o-component.tsx
@@ -17,24 +17,12 @@ import { columns } from './filter-o-colomns';
 export default function SalesComponent () {
     const [ ordersItems, setOrdersItems ] = useState<OrderItem[]>( [] );
 
-    // const getOrders = async () => {
-
-    //     const allOrdersItems = await axiosInstanceClient.get(
-    //         `${ process.env.NEXT_PUBLIC_BACKEND_URL }/OrderItem`,
-    //     );
-
-    //     console.log( allOrdersItems.data )
-    //     const orderItemsData = allOrdersItems.data.sort( ( a: any, b: any ) => b.quantityOrdered - a.quantityOrdered )
-    //     setOrdersItems( orderItemsData )
-    // };
-
     const getOrders = async () => {
         try {
             const allOrdersItems = await axiosInstanceClient.get(
                 `${ process.env.NEXT_PUBLIC_BACKEND_URL }/OrderItem`
             );
 
-            // Process the data to merge items with the same title
             const mergedItemsMap = new Map();
 
             allOrdersItems.data.forEach( ( item: { title: any; quantityOrdered: any; } ) => {
@@ -42,18 +30,14 @@ export default function SalesComponent () {
                     const existingItem = mergedItemsMap.get( item.title );
                     existingItem.quantityOrdered += item.quantityOrdered;
                 } else {
-                    // Clone the item to avoid mutating the original data directly
                     mergedItemsMap.set( item.title, { ...item } );
                 }
             } );
 
-            // Convert the Map back to an array
             const mergedItemsArray = Array.from( mergedItemsMap.values() );
 
-            // Sort the merged array by quantityOrdered
             const orderItemsData = mergedItemsArray.sort( ( a, b ) => b.quantityOrdered - a.quantityOrdered );
 
-            // Update the state
             setOrdersItems( orderItemsData );
         } catch ( error ) {
             console.error( 'Error fetching order items:', error );
