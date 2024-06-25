ef3470a4cb5ceb5665a0632782785cedbc480f76
Godwin <agblezeg@gmail.com>
2024-06-25 09:31:25 +0200

    Add timeout with react-idle-timer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,65 @@
-import type { Metadata } from 'next';
+'use client';
+
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { useIdleTimer } from 'react-idle-timer';
+import { useEffect, useState } from 'react';
+import { removeTokenCookie } from '@/lib/cookies';
+import { useRouter } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'CargoConnect',
-  description: 'Progetto ITS',
-  icons: '/favicon.ico'
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [state, setState] = useState<string>('Active');
+  const [count, setCount] = useState<number>(0);
+  const [remaining, setRemaining] = useState<number>(0);
+  const router = useRouter();
+
+  const onIdle = () => {
+    setState('Idle');
+  };
+
+  const onActive = () => {
+    setState('Active');
+  };
+
+  const onAction = () => {
+    setCount(count + 1);
+  };
+
+  const { getRemainingTime } = useIdleTimer({
+    onIdle,
+    onActive,
+    onAction,
+    timeout: 900000_000, // 15 minutes in milliseconds
+    throttle: 500,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newRemaining = Math.ceil(getRemainingTime() / 1000);
+
+      setRemaining(newRemaining);
+
+      if (newRemaining === 0) {
+        removeTokenCookie();
+        router.push('/signin');
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <html lang="en">
+      <meta charSet="utf-8" />
+      <title>CargoConnect</title>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
